2e8029255b5f1653424c781e63b5604b80423a5b
עומרי מזרחי <omriom5@gmail.com>
2022-04-19 21:31:57 +0300

    finished easy bonus feature

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,24 +6,33 @@ public class PlayerController : MonoBehaviour
     public float speed = 20f;
     public Transform leftWall;
     public Transform rightWall;
+    public Transform topWall;
+    public Transform bottomWall;
     [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
     private float leftBoundaryX;
     private float rightBoundaryX;
+    private float topBoundaryZ;
+    private float bottomBoundaryZ;
     #endregion
 
     void Start() {
         leftBoundaryX = leftWall.position.x;
         rightBoundaryX = rightWall.position.x;
+        topBoundaryZ = topWall.position.z;
+        bottomBoundaryZ = bottomWall.position.z;
     }   
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
 
     void FixedUpdate() {
-        transform.Translate(Vector3.right * (horizontalInput * speed * Time.fixedDeltaTime));
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * (speed * Time.fixedDeltaTime));
         float fixedPosX = Mathf.Clamp(transform.position.x, leftBoundaryX, rightBoundaryX);
-        transform.position = new Vector3(fixedPosX, transform.position.y, transform.position.z);
+        float fixedPosZ = Mathf.Clamp(transform.position.z, bottomBoundaryZ, topBoundaryZ);
+        transform.position = new Vector3(fixedPosX, transform.position.y, fixedPosZ);
     }
 }
