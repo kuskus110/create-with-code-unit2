1db072f50f0221e2a0f99d383b8e199af498d6f1
עומרי מזרחי <omriom5@gmail.com>
2022-04-19 21:12:51 +0300

    finished challange - fetch

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spamDelay = .5f;
+    private float shootCooldown = 0;
+    
+    void Update()
+    {
+        shootCooldown -= Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && shootCooldown <= 0)
+        {
+            shootCooldown = spamDelay;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float spawnCooldown;
+    
+    void Start()
+    {
+        ResetSpawnCooldown();
+    }
+
+    void Update() {
+        spawnCooldown -= Time.deltaTime;
+        if (spawnCooldown <= 0) {
+            ResetSpawnCooldown();
+            SpawnRandomBall();
+        }
+    }
+
+    void ResetSpawnCooldown() {
+        spawnCooldown = Random.Range(.5f, 2f);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int rndIndex = Random.Range(0, ballPrefabs.Length);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[rndIndex], spawnPos, ballPrefabs[0].transform.rotation);
+    }
+
+}
